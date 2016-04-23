5beb35be4222678c1ec92e9b57564eb4a4f3f48c
Zhuyi Xue <alfred532008@gmail.com>
2016-04-23 15:27:25 -0700

    used sphinx_rtd_theme for doc

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,8 @@ sys.path.append(os.path.abspath('..'))
 
 import setup as setup_script
 
+import sphinx_rtd_theme
+
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
@@ -97,7 +99,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'nature'
+# html_theme = 'nature'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -105,7 +108,8 @@ html_theme = 'nature'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_theme']
+# html_theme_path = ['_theme']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
